eb0b0e8c98141f1693d884fab2dd6eefb8966be0
Liam Marshall <marshl@live.com.au>
2017-06-06 09:10:35 +1000

    Added a new widget to display patient diagnoses and medications (with toggle actions)

---
+++ protected/components/views/patientDiagnosesAndMedicationsWidget.php
@@ -0,0 +1,80 @@
+
+
+<?php if ($this->patient->secondarydiagnoses): ?>
+    <div class="row data-row">
+        <div class="large-12 column">
+            <div>Diagnoses:
+                <a href="javascript:void(0)"
+                   data-show-label="show" data-hide-label="hide"
+                   onclick="toggleSection(this, '#collapse-section_<?php echo $this->patient->id . '_diagnosis'; ?>');">show
+                </a>
+            </div>
+            <div id="collapse-section_<?php echo $this->patient->id . '_diagnosis'; ?>" style="display:none">
+                <div class="diagnoses detail row data-row">
+                    <div class="large-12 column">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Diagnosis</th>
+                                <th>Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($this->patient->secondarydiagnoses as $diagnosis): ?>
+                                <tr>
+                                    <td><?php echo $diagnosis->disorder->fully_specified_name; ?></td>
+                                    <td><?php echo $diagnosis->dateText; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
+
+<?php if ($this->patient->medications): ?>
+    <div class="row data-row">
+        <div class="large-12 column">
+            <div>Medications:
+                <a href="javascript:void(0)"
+                   data-show-label="show" data-hide-label="hide"
+                   onclick="toggleSection(this, '#collapse-section_<?php echo $this->patient->id . '_medication'; ?>');">show
+                </a>
+            </div>
+
+            <div id="collapse-section_<?php echo $this->patient->id . '_medication'; ?>" style="display:none">
+                <div class="medications detail row data-row">
+                    <div class="large-12 column">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Medication</th>
+                                <th>Administration</th>
+                                <th>Date From</th>
+                                <th>Date To</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($this->patient->medications as $medication): ?>
+                                <tr>
+                                    <td><?php echo $medication->getDrugLabel(); ?></td>
+                                    <td><?= $medication->dose ?>
+                                        <?= isset($medication->route->name) ? $medication->route->name : '' ?>
+                                        <?= $medication->option ? "({$medication->option->name})" : '' ?>
+                                        <?= isset($medication->frequency->name) ? $medication->frequency->name : '' ?></td>
+                                    <td><?php echo Helper::formatFuzzyDate($medication->start_date); ?></td>
+                                    <td><?php echo isset($medication->end_date) ? Helper::formatFuzzyDate($medication->end_date) : ''; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
