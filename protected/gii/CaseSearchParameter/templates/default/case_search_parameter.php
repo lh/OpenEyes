b896c1ebef335496ea6fb5f2b5e916dad9b5ef44
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-06-08 15:05:28 +1000

    Subproject commit 7d8493ff4dbba6cfafd24489c142943111361dca Now using PHP scripting in template for renderParameter function.

--- protected/gii/CaseSearchParameter/templates/default/case_search_parameter.php
+++ protected/gii/CaseSearchParameter/templates/default/case_search_parameter.php
@@ -50,12 +50,17 @@ foreach (explode(',', $this->attributeList) as $attribute):?>
 
     public function renderParameter($id)
     {
-        // Place screen-rendering code here.
+        // Initialise any rendering variables here.
+        ?>
+        <!-- Place screen-rendering code here. -->
+        <?php
+        echo '<?php'; ?>
+
     }
 
     /**
     * Generate a SQL fragment representing the subquery of a FROM condition.
-    * @param $searchProvider The search provider. This is used to determine whether or not the search provider is using SQL syntax.
+    * @param $searchProvider SearchProvider The search provider. This is used to determine whether or not the search provider is using SQL syntax.
     * @return mixed The constructed query string.
     */
     public function query($searchProvider)
@@ -81,10 +86,10 @@ foreach (explode(',', $this->attributeList) as $attribute):?>
 
     /**
     * Generate a SQL fragment representing a JOIN condition to a subquery.
-    * @param $joinAlias The alias of the table being joined to.
-    * @param $criteria An array of join conditions. The ID for each element is the column name from the aliased table.
-    * @param $searchProvider The search provider. This is used for an internal query invocation for subqueries.
-    * @return string A SQL string representing a complete join condition. Join type is specified within the subclass definition.
+    * @param $joinAlias string The alias of the table being joined to.
+    * @param $criteria array An array of join conditions. The ID for each element is the column name from the aliased table.
+    * @param $searchProvider SearchProvider The search provider. This is used for an internal query invocation for subqueries.
+    * @return mixed A SQL string representing a complete join condition. Join type is specified within the subclass definition.
     */
     public function join($joinAlias, $criteria, $searchProvider)
     {
