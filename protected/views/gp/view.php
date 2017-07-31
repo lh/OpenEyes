27a8e04094e3587b118e1da2ff8b6508029a636e
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-07-31 14:11:05 +1000

    Fixed formatting of labels and display boxes to bring them in-line with the patient details screen.

--- protected/views/gp/view.php
+++ protected/views/gp/view.php
@@ -8,36 +8,34 @@ $this->pageTitle = 'View Practitioner';
 <h1 class="badge">Practitioner Summary</h1>
 <div class="row data-row">
   <div class="large-8 column">
-    <div class="row data-row">
-      <section class="box patient-info patient-contacts js-toggle-container">
-        <h3 class="box-title">Contact Information:</h3>
-        <a href="#" class="toggle-trigger toggle-hide js-toggle">
+    <section class="box patient-info js-toggle-container">
+      <h3 class="box-title">Contact Information:</h3>
+      <a href="#" class="toggle-trigger toggle-hide js-toggle">
             <span class="icon-showhide">
                 Show/hide this section
             </span>
-        </a>
-        <div class="js-toggle-body">
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::label('Name', null); ?>
-            </div>
-            <div class="large-4 column end">
-              <div class="data-value"><?php echo CHtml::encode($model->getCorrespondenceName()); ?></div>
-            </div>
+      </a>
+      <div class="js-toggle-body">
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Name:</div>
           </div>
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::activeLabelEx($model->contact, 'primary_phone'); ?>
-            </div>
-            <div class="large-3 column end">
-              <div
-                  class="data-value"><?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?></div>
-            </div>
+          <div class="large-4 column end">
+            <div class="data-value"><?php echo CHtml::encode($model->getCorrespondenceName()); ?></div>
           </div>
-          <!--Add the address row here when GPs get tied directly to practices rather than through patient records.-->
         </div>
-      </section>
-    </div>
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Phone Number:</div>
+          </div>
+          <div class="large-3 column end">
+            <div
+                class="data-value"><?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?></div>
+          </div>
+        </div>
+        <!--Add the address row here when GPs get tied directly to practices rather than through patient records.-->
+      </div>
+    </section>
   </div>
     <?php if (Yii::app()->user->checkAccess('TaskCreateGp')): ?>
       <div class="large-4 column end">
--- protected/views/practice/view.php
+++ protected/views/practice/view.php
@@ -8,55 +8,53 @@ $this->pageTitle = 'View Practice';
 <h1 class="badge">Practice Summary</h1>
 <div class="row data-row">
   <div class="large-8 column">
-    <div class="row data-row">
-      <section class="box patient-info patient-contacts js-toggle-container">
-        <h3 class="box-title">Contact Information:</h3>
-        <a href="#" class="toggle-trigger toggle-hide js-toggle">
+    <section class="box patient-info js-toggle-container">
+      <h3 class="box-title">Contact Information:</h3>
+      <a href="#" class="toggle-trigger toggle-hide js-toggle">
             <span class="icon-showhide">
                 Show/hide this section
             </span>
-        </a>
-        <div class="js-toggle-body">
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::label('Practice Contact', null); ?>
-            </div>
-            <div class="large-3 column end">
-              <div
-                  class="data-value"><?php echo CHtml::encode($model->contact->getFullName()); ?>
-              </div>
-            </div>
+      </a>
+      <div class="js-toggle-body">
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Practice Contact:</div>
           </div>
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::label('Practice Address', null); ?>
-            </div>
-            <div class="large-4 column end">
-              <div class="data-value"><?php echo CHtml::encode($model->getAddressLines()); ?></div>
-            </div>
-          </div>
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::label('Code', null); ?>
-            </div>
-            <div class="large-3 column end">
-              <div
-                  class="data-value"><?php echo CHtml::encode($model->code); ?>
-              </div>
-            </div>
-          </div>
-          <div class="row data-row">
-            <div class="large-3 column">
-                <?php echo CHtml::activeLabelEx($model->contact, 'primary_phone'); ?>
-            </div>
-            <div class="large-3 column end">
-              <div
-                  class="data-value"><?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?></div>
+          <div class="large-3 column end">
+            <div
+                class="data-value"><?php echo CHtml::encode($model->contact->getFullName()); ?>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Practice Address:</div>
+          </div>
+          <div class="large-4 column end">
+            <div class="data-value"><?php echo CHtml::encode($model->getAddressLines()); ?></div>
+          </div>
+        </div>
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Code:</div>
+          </div>
+          <div class="large-3 column end">
+            <div
+                class="data-value"><?php echo CHtml::encode($model->code); ?>
+            </div>
+          </div>
+        </div>
+        <div class="row data-row">
+          <div class="large-3 column">
+            <div class="data-label">Phone Number:</div>
+          </div>
+          <div class="large-3 column end">
+            <div
+                class="data-value"><?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?></div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
     <?php if (Yii::app()->user->checkAccess('TaskCreatePractice')): ?>
       <div class="large-4 column end">
