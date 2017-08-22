55fa4cc1ec975d9db2577752287a5c651520af29
Liam Marshall <marshl@live.com.au>
2017-08-22 14:02:55 +1000

    Changed the heading of the Edit Practice screen from "Practitioner" to "Practice"

--- protected/views/practice/update.php
+++ protected/views/practice/update.php
@@ -8,7 +8,7 @@ $this->pageTitle = 'Update Practice';
 <div class="box content admin-content">
   <div class="large-10 column content admin large-centered">
     <div class="box admin">
-      <h1 class="text-center">Update Practitioner Details</h1>
+      <h1 class="text-center">Update Practice Details</h1>
         <?php $this->renderPartial('_form', array('model' => $model, 'address' => $address, 'contact' => $contact)); ?>
     </div>
   </div>
