06b1ce87f78e2556a17bc99decf01e706de080ba
Vegemash <mrisaacjackson@gmail.com>
2017-09-22 14:59:53 +1000

    Added unit test for unique nhs number and hos(CERA) number

---
+++ protected/tests/unit/models/AUSPatientTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 22/09/17
+ * Time: 2:11 PM
+ */
+
+class AUS_PatientTest extends CDbTestCase
+{
+    protected $fixtures = array('patient' => 'Patient');
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testUniqueMedicareNo()
+    {
+        //create a patient as a duplicate of one in the database
+        $patient_dup = new Patient;
+        $patient_dup->attributes = $this->patient['patient1'];
+
+        //change the things we don't want it to fail on
+        $patient_dup->id = NULL;
+        $patient_dup->pas_key = NULL;
+        $patient_dup->hos_num = 65465465;
+
+        //fail to save with duplication
+        $this->assertFalse($patient_dup->save());
+
+        //remove duplication and save successfully
+        $patient_dup->nhs_num = 65465445;
+        $this->assertTrue($patient_dup->save());
+    }
+
+    public function testUniqueHosNo()
+    {
+        //create a patient as a duplicate of one in the database
+        $patient_dup = new Patient;
+        $patient_dup->attributes = $this->patient['patient1'];
+
+        //change the things we don't want it to fail on
+        $patient_dup->id = NULL;
+        $patient_dup->pas_key = NULL;
+        $patient_dup->nhs_num = 65465465;
+
+        //fail to save with duplication
+        $this->assertFalse($patient_dup->save());
+
+        //remove duplication and save successfully
+        $patient_dup->hos_num = 65465445;
+        $this->assertTrue($patient_dup->save());
+    }
+}
