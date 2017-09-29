f654878395bce30f7be46672e7e4c5a0fed7ece2
Liam Marshall <marshl@live.com.au>
2017-09-29 10:55:47 +1000

    OE-278: Changed the field type of the OphCoTherapyapplication_sender_email setting item from "Radio buttons" to "Text Field"

--- protected/modules/OphCoTherapyapplication/migrations/m170711_132400_add_system_setting_therapy_sender.php
+++ protected/modules/OphCoTherapyapplication/migrations/m170711_132400_add_system_setting_therapy_sender.php
@@ -4,7 +4,7 @@ class m170711_132400_add_system_setting_therapy_sender extends CDbMigration
 {
     public function up()
     {
-        $this->insert('setting_metadata', array('display_order' => 0, 'field_type_id' => 3, 'key' => 'OphCoTherapyapplication_sender_email', 'name' => 'Therapy Application Sender Email'));
+        $this->insert('setting_metadata', array('display_order' => 0, 'field_type_id' => 4, 'key' => 'OphCoTherapyapplication_sender_email', 'name' => 'Therapy Application Sender Email'));
         $this->insert('setting_installation', array('key' => 'OphCoTherapyapplication_sender_email', 'value' => 'therapyapps@openeyes'));
     }
 
