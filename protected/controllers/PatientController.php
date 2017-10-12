c207d053972408cc7a379ca88fd4709074360ad1
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-10-12 15:01:10 +1100

    OE-323: Practice name field will now ignore case when searching for autocomplete results.

--- protected/controllers/PatientController.php
+++ protected/controllers/PatientController.php
@@ -1845,7 +1845,7 @@ class PatientController extends BaseController
         $criteria->addCondition('( (date_end is NULL OR date_end > NOW()) AND (date_start is NULL OR date_start < NOW()))');
         
         $criteria->addSearchCondition('LOWER(CONCAT_WS(", ", address1, address2, city, county, postcode))', $term);
-        $criteria->addSearchCondition('first_name', $term, true, 'OR');
+        $criteria->addSearchCondition('LOWER(first_name)', strtolower($term), true, 'OR');
         
         $practices = Practice::model()->findAll($criteria);
         
