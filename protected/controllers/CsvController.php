32bb62616cab2236fdbfe90adcbdbf46242dda39
Unknown <mrisaacjackson@gmail.com>
2017-12-11 16:23:51 +1100

    Added diagnoses, referred to, GP, Optom, Opthal

--- protected/controllers/CsvController.php
+++ protected/controllers/CsvController.php
@@ -187,7 +187,6 @@ class CsvController extends BaseController
             array('var_name' => 'dob'                           , 'default' => null,),
             array('var_name' => 'gender'                        , 'default' => 'U',),
             array('var_name' => 'nhs_num'                       , 'default' => null,),
-            array('var_name' => 'gp_id'                         , 'default' => null,),
             array('var_name' => 'date_of_death'                 , 'default' => null,),
             array('var_name' => 'practice_id'                   , 'default' => null,),
             array('var_name' => 'ethnic_group_id'               , 'default' => null,),
@@ -209,6 +208,210 @@ class CsvController extends BaseController
 
         if(!$new_patient->save()){
             return $new_patient->getErrors();
+        }
+        //patient contact assignments
+
+        //referred to
+        if(isset($patient['referred_to_first_name']) and isset($patient['referred_to_last_name'])){
+            $referred_to = null;
+            //Find if exists
+            $referred_to = User::model()->findByAttributes(array(
+                'first_name' => $patient['referred_to_first_name'],
+                'last_name' => $patient['reffered_to_last_name'],
+            ));
+            if($referred_to === null){
+                $errors[] = 'Cannot find referred to user';
+                return $errors;
+            }
+            $pat_ref = new PatientUserReferral();
+            $pat_ref->user = $referred_to;
+            if(!$pat_ref->save()){
+                $errors[] = 'Count not save referred to user';
+                array_unshift($errors, $pat_ref->getErrors());
+                return $errors;
+            }
+        }
+
+        //optom
+        if(isset($patient['optom_first_name']) and isset($patient['optom_last_name'])){
+            $optom_label = ContactLabel::model()->findByAttributes(array('name' => 'Optometrist'));
+            $optom_contact = Contact::model()->findByAttributes(array(
+                'contact_label_id' => $optom_label->id,
+                'first_name' => $patient['optom_first_name'],
+                'last_name' => $patient['optom_last_name'],
+            ));
+            if($optom_contact === null){
+                $optom_contact = new Contact();
+                $optom_contact->first_name = $patient['optom_first_name'];
+                $optom_contact->last_name = $patient['optom_last_name'];
+                $optom_contact->label = $optom_label;
+                if(!$optom_contact->save()){
+                    $errors[] = 'Could not save new optometrist contact';
+                    array_unshift($errors, $optom_contact->getErrors());
+                    return $errors;
+                }
+            }
+            $pat_con = new PatientContactAssignment();
+            $pat_con->contact = $optom_contact;
+            if (!$pat_con->save()){
+                $errors[] = 'Could not save optometrist contact assignment';
+                array_unshift($errors, $pat_con->getErrors());
+                return $errors;
+            }
+            $new_gp = new Gp();
+            $new_gp->obj_prof = 0;
+            $new_gp->nat_id = 0;
+            $new_gp->contact = $pat_con;
+            if(!$new_gp->save()){
+                $errors[] = 'Could not save new practitioner contact';
+                array_unshift($errors, $new_gp->getErrors());
+                return $errors;
+            }
+        }
+
+        //opthal
+        if(isset($patient['opthal_first_name']) and isset($patient['opthal_last_name'])){
+            $opthal_label = ContactLabel::model()->findByAttributes(array('name' => 'Ophthalmologist'));
+            $opthal_contact = Contact::model()->findByAttributes(array(
+                'contact_label_id' => $opthal_label->id,
+                'first_name' => $patient['opthal_first_name'],
+                'last_name' => $patient['opthal_last_name'],
+            ));
+            if($opthal_contact === null){
+                $opthal_contact = new Contact();
+                $opthal_contact->first_name = $patient['opthal_first_name'];
+                $opthal_contact->last_name = $patient['opthal_last_name'];
+                $opthal_contact->label = $opthal_label;
+                if(!$opthal_contact->save()){
+                    $errors[] = 'Could not save new ophthalmologist contact';
+                    array_unshift($errors, $opthal_contact->getErrors());
+                    return $errors;
+                }
+            }
+            $pat_con = new PatientContactAssignment();
+            $pat_con->contact = $opthal_contact;
+            if (!$pat_con->save()){
+                $errors[] = 'Could not save ophthalmologist contact assignment';
+                array_unshift($errors, $pat_con->getErrors());
+                return $errors;
+            }
+            $new_gp = new Gp();
+            $new_gp->obj_prof = 0;
+            $new_gp->nat_id = 0;
+            $new_gp->contact = $pat_con;
+            if(!$new_gp->save()){
+                $errors[] = 'Could not save new practitioner contact';
+                array_unshift($errors, $new_gp->getErrors());
+                return $errors;
+            }
+        }
+
+        //Gp
+        if(isset($patient['gp_first_name']) and isset($patient['gp_last_name'])){
+            $gp_label = ContactLabel::model()->findByAttributes(array('name' => 'General Practitioner'));
+            $gp_contact = Contact::model()->findByAttributes(array(
+                'contact_label_id' => $gp_label->id,
+                'first_name' => $patient['gp_first_name'],
+                'last_name' => $patient['gp_last_name'],
+            ));
+            if($gp_contact === null){
+                $gp_contact = new Contact();
+                $gp_contact->first_name = $patient['gp_first_name'];
+                $gp_contact->last_name = $patient['gp_last_name'];
+                $gp_contact->label = $gp_label;
+                if(!$gp_contact->save()){
+                    $errors[] = 'Could not save new GP contact';
+                    array_unshift($errors, $gp_contact->getErrors());
+                    return $errors;
+                }
+                $new_gp = new Gp();
+                $new_gp->obj_prof = 0;
+                $new_gp->nat_id = 0;
+                $new_gp->contact = $gp_contact;
+                if(!$new_gp->save()){
+                    $errors[] = 'Could not save new practitioner contact';
+                    array_unshift($errors, $new_gp->getErrors());
+                    return $errors;
+                }
+            }
+            $pat_con = new PatientContactAssignment();
+            $pat_con->contact = $gp_contact;
+            if (!$pat_con->save()){
+                $errors[] = 'Could not save general practitioner contact';
+                array_unshift($errors, $pat_con->getErrors());
+                return $errors;
+            }
+        }
+        
+        //diagnoses
+        if(isset($patient['LE_diagnosis']) || isset($patient['RE_diagnosis'])){
+            $context = Firm::model()->findByAttributes(array(
+                'name' => isset($patient['context']) ? $patient['context'] :  'Medical Retinal firm'
+            ));
+            $episode = new Episode();
+            $episode->firm = $context;
+            if(!$episode->save()){
+                $errors[] = 'Could not save new episode';
+                array_unshift($errors, $episode->getErrors());
+                return $errors;
+            }
+
+            $event = new Event();
+            $event->event_type_id = EventType::model()->findByAttributes(array(
+                'name' => 'Examination'
+            ))->id;
+            $event->episode = $episode;
+
+            if(!$event->save()){
+                $errors[] = 'Could not save new event';
+                array_unshift($errors, $event->getErrors());
+                return $errors;
+            }
+
+            $element = new \OEModule\OphCiExamination\models\Element_OphCiExamination_Diagnoses();
+            $element->event = $event;
+
+            if(!$element->save()){
+                $errors[] = 'could not save diagnoses element';
+                array_unshift($errors, $element->getErrors());
+            }
+
+            if(isset($patient['LE_diagnosis'])){
+                $disorder = Disorder::model()->findByAttributes(array(
+                    'fully_specified_name' => $patient['LE_diagnosis']
+                ));
+                if($disorder === null){
+                    $errors[] = 'Could not find left eye diagnosis '.$patient['LE_diagnosis'];
+                    return $errors;
+                }
+                $diagnosis = new \OEModule\OphCiExamination\models\OphCiExamination_Diagnosis();
+                $diagnosis->element_diagnoses_id = $element->id;
+                $diagnosis->disorder_id = $disorder->id;
+                $diagnosis->eye_id = Eye::LEFT;
+                if(!$diagnosis->save()){
+                    $errors[] = 'Could not save left eye diagnosis';
+                    array_unshift($errors, $diagnosis->getErrors());
+                    return $errors;
+                }
+            }
+            if(isset($patient['RE_diagnosis'])){
+                $disorder = Disorder::model()->findByAttributes(array(
+                    'fully_specified_name' => $patient['LE_diagnosis']
+                ));
+                if($disorder === null){
+                    $errors[] = 'Could not find right eye diagnosis '.$patient['RE_diagnosis'];
+                    return $errors;
+                }
+                $diagnosis = new \OEModule\OphCiExamination\models\OphCiExamination_Diagnosis();
+                $diagnosis->element_diagnoses_id = $element->id;
+                $diagnosis->disorder_id = $disorder->id;
+                $diagnosis->eye_id = Eye::RIGHT;
+                if(!$diagnosis->save()){
+                    $errors[] = 'Could not save right eye diagnosis';
+                    array_unshift($errors, $diagnosis->getErrors());
+                    return $errors;
+                }
+            }
         }
 
         return $errors;
