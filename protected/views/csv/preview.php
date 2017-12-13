6511f22796dcb8a8aed67306b2283f3bf85f43f9
Unknown <mrisaacjackson@gmail.com>
2017-12-13 15:03:23 +1100

    Added a scrollable div to the preview table for long tables

--- protected/views/csv/preview.php
+++ protected/views/csv/preview.php
@@ -9,24 +9,26 @@ $form = $this->beginWidget(
     )
 );
 if (!empty($table)): ?>
+  <div style="overflow: auto">
     <table>
-        <tr>
-            <?php foreach (array_keys($table[0]) as $header): ?>
-                <th>
-                    <?php echo $header; ?>
-                </th>
-            <?php endforeach; ?>
-        </tr>
+      <tr>
+          <?php foreach (array_keys($table[0]) as $header): ?>
+            <th>
+                <?php echo $header; ?>
+            </th>
+          <?php endforeach; ?>
+      </tr>
         <?php foreach ($table as $row): ?>
-            <tr>
-                <?php foreach ($row as $column): ?>
-                    <td>
-                        <?php echo $column ?>
-                    </td>
-                <?php endforeach; ?>
-            </tr>
+          <tr>
+              <?php foreach ($row as $column): ?>
+                <td>
+                    <?php echo $column ?>
+                </td>
+              <?php endforeach; ?>
+          </tr>
         <?php endforeach; ?>
     </table>
+  </div>
 <?php endif;
 echo CHtml::submitButton('Import');
 $this->endWidget();
